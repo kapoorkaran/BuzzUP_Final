ea41786bdba16187f9cc0efe66fed8b373be4f1e
ashwin.5.jayadev <ashwin.5.jayadev@stackroute.in>
2018-10-14 02:49:31 +0530

    final

---
+++ paymentservice/src/main/java/com/stackroute/buzzup/paymentservice/PaymentserviceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.buzzup.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentserviceApplication.class, args);
+	}
+}
